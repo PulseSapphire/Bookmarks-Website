0b93d9172c4834cf3ff742677f38e9a49fdebf45
PulseSapphire <souravdube17122003@gmail.com>
2023-09-04 04:57:57 +0530

    Styled the CommunityJoinMessage component.
    Worked on the styles of the CommunityJoinMessage component and finished it.

--- bookmarks-website/src/components/CommunityJoinMessage/CommunityJoinMessage.js
+++ bookmarks-website/src/components/CommunityJoinMessage/CommunityJoinMessage.js
@@ -10,8 +10,10 @@ function CommunityJoinMessage() {
         <div className={styles.container}>
             <div className={styles.sub_container}>
                 <Image src='/website_design/person.png' width={416} height={374} alt='person.' />
-                <h2>Be a part of our great community!</h2>
-                <button>Join Today</button>
+                <div className={styles.info_container}>
+                    <h2>Be a part of our great community!</h2>
+                    <button>Join Today</button>
+                </div>
             </div>
         </div>
     )
