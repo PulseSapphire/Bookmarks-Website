c5ca21ebcf68b803ccc3e4ccfedf38e4e6aca4e3
PulseSapphire <souravdube17122003@gmail.com>
2023-09-04 01:37:48 +0530

    Added the sign up and sign in buttons to header.
    Added and styled sign up and sign in buttons to the header.

--- bookmarks-website/src/components/Header/Header.js
+++ bookmarks-website/src/components/Header/Header.js
@@ -7,9 +7,12 @@ function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.logo_container}>
-
                 <Image src='/website_design/logo.png' width={53} height={40} alt='logo image.' />
                 <h1>sKYLINKER<span>.us</span></h1>
+            </div>
+            <div className={styles.auth_container}>
+                <button className={styles.signin}>Login</button>
+                <button className={styles.signup}>Sign up</button>
             </div>
         </header>
     )
